5723a4e790fd06d5d4bb9d19caa4c13afd952bf5
Lionhatt <gavindxy@gmail.com>
2020-09-07 01:02:28 +1000

    push final finished project

--- javascript/script.js
+++ javascript/script.js
@@ -13,38 +13,50 @@ var gobackButton = document.createElement("button");
 gobackButton.innerHTML = "GO Back";
 var clearButton = document.createElement("button");
 clearButton.innerHTML = "Clear Highscores";
+var i = 0;
 
+// display content of innital page when suer open the html
 window.onload = initPage;
 
 
-
+// the question set for the quiz, user can ater or add questions to the set
 var jsQuestions = [{
     questionText: "Inside which HTML element do we put the JavaScript?",
     answersText: ["<script>", "<javascript>", "<js>", "<scripting>"],
     correctAnswer: "<scripting>"
 }, {
     questionText: "Where is the correct place to insert a JavaScript?",
-    answersText: ["Both the <head> section and the <body> section are correct", "The <head> section", "The <body> section", "the <html> section"],
+    answersText: ["Both the <head> section and the <body> section are correct", "The <head> section", "The <body> section"],
     correctAnswer: "The <body> section"
+},{
+    questionText: "What is the correct syntax for referring to an external script called \"xxx.js\"?",
+    answersText: ["<script href=\"xxx.js\">", "<script src=\"xxx.js\">", "<script name=\"xxx.js\">"],
+    correctAnswer: "<script src=\"xxx.js\">"
+},{
+    questionText: "How do you create a function in JavaScript?",
+    answersText: ["function myFunction()", "function:myFunction()", "function = myFunction()"],
+    correctAnswer: "function myFunction()"
+},{
+    questionText: "How to write an IF statement in JavaScript?",
+    answersText: ["if i = 5", "if i = 5 then", "if i == 5 then", "if(i == 5)"],
+    correctAnswer: "if(i == 5)"
 }]
 
 
-
+//start the quiz
 startButton.addEventListener("click", function () {
     var testduration = false;
     quizStart();
     startTimer();
-
-
 })
 
-//start the count-down timer
+//start the count-down timer, user can change the timeinterval
 function startTimer() {
     timeLeft = 75;
     var timeInterval = setInterval(function () {
         timer.textContent = "time: " + timeLeft;
         timeLeft--;
-        if (timeLeft === 0) {
+        if (timeLeft === 0 || i === jsQuestions.length) {
             clearInterval(timeInterval);
             clearContent();
             scoreBoard();
@@ -60,6 +72,7 @@ function clearContent() {
 
 }
 
+// function to display the score board content on the page 
 function scoreBoard() {
     question.textContent = "All done!";
     answers.textContent = "Your final score is " + timeLeft;
@@ -75,7 +88,7 @@ submitButton.addEventListener("click", highScoresPage);
 
 highScoresLink.addEventListener("click", highScoresPage);
 
-
+// button to clear the local storage
 clearButton.addEventListener("click", function () {
     localStorage.clear();
     answers.textContent = ""
@@ -87,8 +100,9 @@ gobackButton.addEventListener("click", function () {
     initPage();
 });
 
-
+//function for the content of the initial page 
 function initPage() {
+    i = 0;
     clearContent();
     highScoresLink.textContent = "View Highscores";
     timer.textContent = "time: 0";
@@ -97,6 +111,7 @@ function initPage() {
     result.append(startButton);
 }
 
+//function to display the content of the high socres page 
 function highScoresPage() {
     event.preventDefault();
     clearContent();
@@ -124,31 +139,34 @@ function highScoresPage() {
 
 }
 
+//function to use the array of questionsets to form the quiz and button 
 function quizStart() {
-    counter = 0;
-    console.log(counter);
+
     clearContent();
-    question.textContent = jsQuestions[counter].questionText;
-    for (let i = 0; i < jsQuestions[counter].answersText.length; i++) {
+    question.textContent = jsQuestions[i].questionText;
+    for (let k = 0; k < jsQuestions[i].answersText.length; k++) {
         const answerButton = document.createElement("button");
-        answerButton.textContent = jsQuestions[counter].answersText[i];
+        answerButton.textContent = jsQuestions[i].answersText[k];
         answerButton.classList.add("answerButton");
+
+        answers.appendChild(answerButton);
         answerButton.addEventListener("click", function () {
             console.log(answerButton.textContent);
-            console.log(jsQuestions[counter].correctAnswer);
-            if (answerButton.textContent === jsQuestions[counter].correctAnswer) {
+            console.log(jsQuestions[i].correctAnswer);
+            if (answerButton.textContent === jsQuestions[i].correctAnswer) {
                 result.textContent = "Correct!";
+                
             } else {
                 result.textContent = "Incorrect!";
                 timeLeft = timeLeft - 10;
                 console.log(timeLeft);
+
             }
+            setTimeout(function () {
+                i++;
+                return quizStart();
+            },1000)
         })
-        answers.appendChild(answerButton);
-
-
-
     }
-
-
 }
+
